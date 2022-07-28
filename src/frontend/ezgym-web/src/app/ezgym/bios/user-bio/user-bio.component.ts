a8f744b61661a57142d889fdf65522b1ba30f175
Gean Silva <geanalexandre@outlook.com>
2022-07-28 02:44:21 -0300

    added navbar

---
+++ src/frontend/ezgym-web/src/app/ezgym/bios/user-bio/user-bio.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'account-bio',
+    templateUrl: 'user-bio.component.html'
+})
+
+export class UserBioComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
